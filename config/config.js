be4023bfe23e6e913d67a28e99d2c92a6f1091b2
Matthew Thompson <matthew.thompson.a@gmail.com>
2020-08-15 00:41:00 +0000

    updates config for Heroku Postgres

--- config/config.js
+++ config/config.js
@@ -1,30 +1,40 @@
 require("dotenv").config();
 
-const { DB_HOST, DB_USER, DB_PASS, DB_PORT, DB_NAME } = process.env;
+const { DB_HOST, DB_USER, DB_PASS, DB_PORT, DB_URL, DB_NAME } = process.env;
 
+// HEROKU POSTGRES CONFIG
 module.exports = {
   development: {
     host: DB_HOST,
     port: DB_PORT,
     username: DB_USER,
     password: DB_PASS,
-    database: 'sequalize_dev',
+    database: DB_NAME,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        // Ref.: https://github.com/brianc/node-postgres/issues/2009
+        rejectUnauthorized: false,
+      },
+      keepAlive: true,        
+    },      
+    ssl: true,
   },
-  test: {
-    host: DB_HOST,
-    port: DB_PORT,
-    username: DB_USER,
-    password: DB_PASS,
-    database: "sequelize_test",
-    dialect: "postgres",
-  },
-  production: {
-    host: DB_HOST,
-    port: DB_PORT,
-    username: DB_USER,
-    password: DB_PASS,
-    database: "sequelize_production",
-    dialect: "postgres",
-  },
+  // test: {
+  //   host: DB_HOST,
+  //   port: DB_PORT,
+  //   username: DB_USER,
+  //   password: DB_PASS,
+  //   database: "sequelize_test",
+  //   dialect: "postgres",
+  // },
+  // production: {
+  //   host: DB_HOST,
+  //   port: DB_PORT,
+  //   username: DB_USER,
+  //   password: DB_PASS,
+  //   database: "sequelize_production",
+  //   dialect: "postgres",
+  // },
 };
